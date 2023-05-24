2cf6daf43159e6821cee5575f87c9e853e3082eb
koudyt <digitalniweb@gmail.com>
2023-05-24 12:28:25 +0200

    dateFunctions.ts added

---
+++ functions/dateFunctions.ts
@@ -0,0 +1,36 @@
+/**
+ * @param dateOrMonth number 1-12 or Date object
+ * @param year
+ * @returns number of days in month (if month and year or date not specified then returns number of days in current month)
+ */
+export const getDaysInMonth = (
+	dateOrMonth?: Date | number,
+	year?: number
+): number => {
+	let month: number;
+	if (dateOrMonth === undefined) {
+		dateOrMonth = new Date();
+	}
+	if (dateOrMonth instanceof Date) {
+		month = dateOrMonth.getMonth() + 1;
+		year = dateOrMonth.getFullYear();
+	} else {
+		month = dateOrMonth;
+		if (year === undefined) {
+			throw new Error("Year must be provided if month is provided");
+		}
+	}
+	return new Date(year, month, 0).getDate();
+};
+
+/**
+ *
+ * @param date Date object
+ * @param days number of days to add
+ * @returns
+ */
+export const addDays = (date: Date, days: number): Date => {
+	const result = new Date(date);
+	result.setDate(result.getDate() + days);
+	return result;
+};

34460ee9e77c79a11f20c5cc6e69e4702c52b0e8
koudyt <digitalniweb@gmail.com>
2023-07-29 17:04:45 +0200

    privileges changed to actions

---
+++ variables/actions.ts
@@ -0,0 +1,9 @@
+export const actionNames = [
+	"read",
+	"write",
+	"delete",
+	"register",
+	"makeAnOrder",
+	"premium",
+	"register",
+] as const;
---
+++ variables/authorization.ts
@@ -0,0 +1,8 @@
+export const mainAuthorizationNames = ["admin", "user"] as const;
+export const adminAuthorizationNames = [
+	"superadmin",
+	"owner",
+	"admin",
+] as const;
+
+export const userAuthorizationNames = ["user", "tenant"] as const;

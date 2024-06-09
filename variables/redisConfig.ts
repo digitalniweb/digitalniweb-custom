cf3088047e716bdec808a736f4d20637e1eb4bd2
Mirec <mirectec@gmail.com>
2024-06-09 12:33:29 +0200

    add dotenv

--- variables/redisConfig.ts
+++ variables/redisConfig.ts
@@ -1,6 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 const redisConfig = {
-	host: process.env.REDIS_HOST || "127.0.0.1",
-	port: process.env.REDIS_PORT || 6379,
+    host: process.env.REDIS_HOST || "127.0.0.1",
+    port: process.env.REDIS_PORT || 6379,
 };
 
 export default redisConfig;

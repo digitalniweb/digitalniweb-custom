237ddcfde16b019c5ff165eb83cdb514c08968a3
koudyt <digitalniweb@gmail.com>
2024-09-15 09:55:02 +0200

    getObjectFromArray added

---
+++ functions/getObjectFromArray.ts
@@ -0,0 +1,34 @@
+/**
+ * @returns `object` T
+ */
+export default function getObjectFromArray<T>(
+	wantedValue: any = 1,
+	searchedTreeArray: T[] = [],
+	treeLevel: number = -1,
+	options = {
+		key: "id" as const,
+		children: "children" as const,
+	}
+): T | false {
+	for (let i = 0; i < searchedTreeArray.length; i++) {
+		let obj = searchedTreeArray[i] as any;
+		if (!obj[options.key]) continue;
+		if (obj?.[options.key] == wantedValue) {
+			return searchedTreeArray[i];
+		}
+		if (!obj[options.children]) continue;
+		if (
+			treeLevel != -1
+				? obj[options.children] && obj.treeLevel <= treeLevel
+				: obj[options.children]
+		) {
+			const wantedObject = getObjectFromArray<T>(
+				wantedValue,
+				obj[options.children],
+				treeLevel
+			);
+			if (wantedObject !== false) return wantedObject;
+		}
+	}
+	return false;
+}

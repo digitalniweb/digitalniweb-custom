9352b830c7dda47851878af6f106f4c82362ec3c
koudyt <digitalniweb@gmail.com>
2025-09-08 09:41:43 +0200

    loadModels use again (turned off previously)

--- helpers/loadModels.ts
+++ helpers/loadModels.ts
@@ -1,20 +1,18 @@
-// this works I guess, but I am not using this file
-// models get loaded via dynamic imports of Express routes in "/server/api/index.ts"
+// Preload all models so associations work
 
-// import fs from "fs/promises";
-// import type { microservices } from "../../digitalniweb-types";
-// let microservice = process.env.MICROSERVICE_NAME as microservices;
-// export default async function loadModels() {
-// 	try {
-// 		const files = await fs.readdir(`./server/models/${microservice}`);
-// 		for (const file of files) {
-// 			await import(
-// 				`../../server/models/${microservice}/` +
-// 					file.replace(/\.ts$/, ".js")
-// 			);
-// 		}
-// 	} catch (err) {
-// 		console.error("Error loading models:", err);
-// 	}
-// }
-export default function loadModels() {}
+import fs from "fs/promises";
+import type { microservices } from "../../digitalniweb-types";
+let microservice = process.env.MICROSERVICE_NAME as microservices;
+export default async function loadModels() {
+	try {
+		const files = await fs.readdir(`./server/models/${microservice}`);
+		for (const file of files) {
+			await import(
+				`../../server/models/${microservice}/` +
+					file.replace(/\.ts$/, ".js")
+			);
+		}
+	} catch (err) {
+		console.error("Error loading models:", err);
+	}
+}

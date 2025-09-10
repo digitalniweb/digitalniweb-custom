3c94b82995ed8d2a24e5cf4565d5db38861abcf5
koudyt <digitalniweb@gmail.com>
2025-09-10 13:24:25 +0200

    widgetTextOptionsDefault + css variables added

---
+++ variables/css.ts
@@ -0,0 +1,14 @@
+export const textAlign = ["left", "center", "right", "justify"] as const;
+export const lengthUnits = ["px", "%", "em", "rem", "vw", "vh"] as const;
+export const lengthUnitsGrid = ["fr"] as const;
+
+export const backgroundAttachment = ["initial", "fixed"] as const;
+export const backgroundPosition = [
+	"initial",
+	"top",
+	"right",
+	"bottom",
+	"left",
+	"center",
+] as const;
+export const backgroundRepeat = ["no-repeat", "repeat"] as const;
--- variables/widgets.ts
+++ variables/widgets.ts
@@ -1,7 +1,18 @@
-import type { widgetModels as widgetModelsType } from "../../digitalniweb-types/functionality/widgets";
+import type {
+	widgetModels as widgetModelsType,
+	widgetTextOptions,
+} from "../../digitalniweb-types/functionality/widgets";
 // all widget models
 export const widgetModels = ["WidgetText", "WidgetBanner"] as const;
 
 // models of ArticleWidgets
 export const widgetsModelsArticle: widgetModelsType[keyof widgetModelsType][] =
 	["WidgetText", "WidgetBanner"] as const;
+
+export const widgetTextOptionsDefault: widgetTextOptions = {
+	padding: 0,
+	overlay: false,
+	paddingUnits: "px",
+	showHeading: true,
+	textAlign: "left",
+};
